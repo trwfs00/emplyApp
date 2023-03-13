aeacd04bc5e49d808239ff4006a99d17a1d3d169
9fs.dev <trw.designstyle@gmail.com>
2023-03-13 23:34:37 +0700

    second push

---
+++ app/src/main/java/com/example/emplyapp/JobseekerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.emplyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JobseekerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jobseeker)
+    }
+}
