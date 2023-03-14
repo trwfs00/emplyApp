c7bb3cf7ccbd023b65c6a2d9bf3247635a1ec396
Patcharax <114338121+Patcharax@users.noreply.github.com>
2023-03-15 05:08:07 +0700

    recent home

--- app/src/main/java/com/example/emplyapp/HomeActivity.kt
+++ app/src/main/java/com/example/emplyapp/HomeActivity.kt
@@ -3,16 +3,30 @@ package com.example.emplyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.emplyapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
     private lateinit var session: SessionManager
+
+    var  JobRecentlist = arrayListOf<JobRecent>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
         binding.bottomNav.selectedItemId = R.id.home
+        binding.JobRecentRycyclerView.adapter = HomeAdapter(this.JobRecentlist,applicationContext)
+        binding.JobRecentRycyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        binding.JobRecentRycyclerView.addItemDecoration(
+            DividerItemDecoration(binding.JobRecentRycyclerView.getContext(),
+                DividerItemDecoration.VERTICAL)
+        )
+
 
         session = SessionManager(applicationContext)
         // Read data from the preferences
