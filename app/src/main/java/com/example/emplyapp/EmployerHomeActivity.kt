f0e00f95d45b4816f015f6650a7cefb9b2a538c9
9fs.dev <trw.designstyle@gmail.com>
2023-03-15 08:36:37 +0700

    Merge 1 [No error]

--- app/src/main/java/com/example/emplyapp/EmployerHomeActivity.kt
+++ app/src/main/java/com/example/emplyapp/EmployerHomeActivity.kt
@@ -1,11 +1,36 @@
 package com.example.emplyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.emplyapp.databinding.ActivityEmployerHomeBinding
 
 class EmployerHomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEmployerHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_employer_home)
+        binding = ActivityEmployerHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    it.isChecked = true
+                }
+                R.id.application -> {
+                    var i: Intent = Intent(applicationContext, ApplicationActivity::class.java)
+                    startActivity(i)
+                    finish()
+                    it.isChecked = true
+                }
+                R.id.profile -> {
+                    var i: Intent = Intent(applicationContext, ProfileActivity::class.java)
+                    startActivity(i)
+                    finish()
+                    it.isChecked = true
+                }
+            }
+            true
+        }
     }
 }
